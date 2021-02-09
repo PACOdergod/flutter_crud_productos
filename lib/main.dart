305f2aa5d89243967a6bd1376dd6f27f9e4fef55
paco <paco.33353@gmail.com>
2021-02-08 20:50:29 -0600

    Nuevo archivo bloc/provider.dart para implementar el inheritedWidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_productos/src/bloc/provider.dart';
 import 'package:flutter_productos/src/pages/login_page.dart';
 
 void main() => runApp(MyApp());
@@ -6,12 +7,14 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Material App',
-      initialRoute: 'login',
-      routes: {
-        'login': (BuildContext context) => LoginPage(),
-      },
+    return Provider(
+      child: MaterialApp(
+        title: 'Material App',
+        initialRoute: 'login',
+        routes: {
+          'login': (BuildContext context) => LoginPage(),
+        },
+      ),
     );
   }
 }
