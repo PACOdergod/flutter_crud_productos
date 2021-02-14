36d75fc4c15afe769cf5221cd84f7b7f24ef4578
paco <paco.33353@gmail.com>
2021-02-14 14:47:50 -0600

    ProductosProvider tiene el metodo para llamar a la lista de firebase, se manda a llamar en el homePage

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_productos/src/models/producto_model.dart';
+import 'package:flutter_productos/src/providers/productos_providers.dart';
 // import 'package:flutter_productos/src/bloc/provider.dart';
 
 class HomePage extends StatelessWidget {
+  final productosProvider = new ProductosProvider();
+
   @override
   Widget build(BuildContext context) {
     // final bloc = Provider.of(context);
@@ -10,10 +14,28 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('home'),
       ),
-      body: Container(),
-      floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.ac_unit_sharp),
-          onPressed: () => Navigator.pushNamed(context, 'producto')),
+      body: _crearListado(),
+      floatingActionButton: _botonProductos(context),
     );
+  }
+
+  Widget _crearListado() {
+    return FutureBuilder(
+        future: productosProvider.obtenerProductos(),
+        builder: (BuildContext context, AsyncSnapshot<List<ProductoModel>> snapshot) {
+          if (snapshot.hasData) {
+            return Container();
+          } else {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        });
+  }
+
+  Widget _botonProductos(BuildContext context) {
+    return FloatingActionButton(
+        child: Icon(Icons.ac_unit_sharp),
+        onPressed: () => Navigator.pushNamed(context, 'producto'));
   }
 }
--- lib/src/providers/productos_providers.dart
+++ lib/src/providers/productos_providers.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
 
 import 'package:flutter_productos/src/models/producto_model.dart';
@@ -11,5 +13,26 @@ class ProductosProvider {
     await http.post(url, body: productoModelToJson(producto));
 
     return true;
+  }
+
+  Future<List<ProductoModel>> obtenerProductos() async {
+    final url = '$_url/productos.json';
+    final resp = await http.get(url);
+
+    final Map<String, dynamic> decodedData = json.decode(resp.body);
+
+    if (decodedData == null) return [];
+
+    final productos = List<ProductoModel>();
+
+    decodedData.forEach((id, prod) {
+      final prodTemp = ProductoModel.fromJson(prod);
+      prodTemp.id = id;
+
+      productos.add(prodTemp);
+    });
+    // print(productos[0].id);
+
+    return productos;
   }
 }
